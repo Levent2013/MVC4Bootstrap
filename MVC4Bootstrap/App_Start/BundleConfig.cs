2a41d035107fa0bf7797ce3dca731fa92bedf8fe
Andrew Moorcroft <moorcroftlad+git@hotmail.com>
2013-03-25 14:27:03 +0000

    Created solution

---
+++ MVC4Bootstrap/App_Start/BundleConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Optimization;
+
+namespace MVC4Bootstrap.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/assets/js/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/css").Include(
+                "~/assets/css/bootstrap-responsive.css",
+                "~/assets/css/style.css"));
+        }
+    }
+}
---
+++ MVC4Bootstrap/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace MVC4Bootstrap.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
