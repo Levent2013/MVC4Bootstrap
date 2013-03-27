c44e6acbeb47eac20269c69edcaee9bfae520240
Andrew Moorcroft <moorcroftlad+git@hotmail.com>
2013-03-27 15:00:50 +0000

    fixed css bundles being interpreted as scripts

--- MVC4Bootstrap/App_Start/BundleConfig.cs
+++ MVC4Bootstrap/App_Start/BundleConfig.cs
@@ -7,7 +7,7 @@ namespace MVC4Bootstrap.App_Start
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/assets/js/bootstrap.js"));
-            bundles.Add(new ScriptBundle("~/bundles/css").Include(
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
                 "~/assets/css/bootstrap-responsive.css",
                 "~/assets/css/style.css"));
         }
